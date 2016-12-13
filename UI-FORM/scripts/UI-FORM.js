b869ab5691f9b108e6d2dc6fe5682bb6c2688193
751848178 <751848178@users.noreply.github.com>
2016-12-13 18:15:16 +0800

    Add files via upload
    增加控件的disable状态
    用法：
        正常状态：class="radio"
        禁用状态：class="radio disable"

--- UI-FORM/scripts/UI-FORM.js
+++ UI-FORM/scripts/UI-FORM.js
@@ -1,13 +1,9 @@
 ﻿$(function () {
-	$(document).on("click",".addr",function(){
-		$(this).siblings(".addr").removeClass("active");
-		$(this).toggleClass("active");
-	});
-	$(document).on("click",".radio-box",function(){
+	$(document).on("click",".radio-box:not(.disable)",function(){
 		var formGroup = $(this).attr("form-group");  // logistics
 		$(this).toggleClass("active").siblings(".radio-box[form-group=" + formGroup + "]").removeClass("active");
 	});
-	$(document).on("click",".check-box",function(){$(this).toggleClass("active");});
+	$(document).on("click",".check-box:not(.disable)",function(){$(this).toggleClass("active");});
 	$(".open-addrs").on("click",function(){$(this).parents(".addrs").toggleClass("section");});
 	//$(".").on("click",".",function(){});
 });
