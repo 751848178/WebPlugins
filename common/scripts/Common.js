f4cbedfa08ea12d6b5e04188ca4c2e114a511107
xingbo <751848178@qq.com>
2017-11-27 15:30:51 +0800

    1、UrlHelper.Args方法修改，支持参数是数组，且数组参数为空

--- common/scripts/Common.js
+++ common/scripts/Common.js
@@ -61,7 +61,7 @@
 			var argname = pairs[i].substring(0, pos); // 参数名字
 			var value = pairs[i].substring(pos + 1); // 参数值
 			// 以键值对的形式存放到"args"对象中
-			if(args[argname]){
+			if(args[argname] || args[argname] == ""){
 				if(args[argname] instanceof Array){
 					args[argname].push(decodeURI(value));
 				}else{
